6a37a367e9ce55305da6db6ffd44360999810439
Jaume Anguera <Jaume@n134-p189.eduroam.kth.se>
2018-04-18 10:55:48 +0200

    Create different cases for FINITE and INFINITE MarkovChains

--- Assignment 1/PattRecClasses/project_A1.m
+++ Assignment 1/PattRecClasses/project_A1.m
@@ -3,8 +3,8 @@ clc;
 close all;
 
 % A.1 HMM Signal Source
-
-% General parameters
+%% INFINTE Markov Chain
+% General parameters 
 p_initial = [0.5; 0.5];                   % Initial probability
 A_initial = [0.8 0.2; 0.05 0.95];         % Transition probability
 mc = MarkovChain(p_initial, A_initial);   % State generator
@@ -14,6 +14,13 @@ g1 = DiscreteD([1;0]);                    % Distribution for state = 1
 g2 = DiscreteD([0;1]);                    % Distribution for state = 2
 h  = HMM(mc, [g1; g2]);                   % The HMM
 [X_DiscreteD,S_DiscreteD] = rand(h, 100); % Generate an output sequence
+
+
+%% FINITE Markov Chain
+% General parameters 
+p_initial = [0.5; 0.5];                   % Initial probability
+A_initial = [0.8 0.1 0.1; 0.05 0.85 0.1]; % Transition probability
+mc = MarkovChain(p_initial, A_initial);   % State generator
 
 % Define HMM with Gaussian output
 g1 = GaussD('Mean',[0;1],'Covariance',[1,0;0,1]);  % Distribution for state = 1
