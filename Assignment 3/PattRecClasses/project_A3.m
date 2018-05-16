8b67a616f12798b8ff2940098f7dc0ecabd37a79
Jaume Anguera <Jaume@MacBook-MacBook-Pro-de-Jaume-2.local>
2018-05-16 23:31:27 +0200

    Create test cases for the third assignment

---
+++ Assignment 3/PattRecClasses/project_A3.m
@@ -0,0 +1,46 @@
+
+clc;
+close all;
+
+% Third assignment Pattern Recognition and Machine Learning Project
+% Authors: Ashutosh Vaishnav and Jaume Anguera Peris
+% A.3 Algorithm Implementation
+
+
+%% ---------------- Forward Algorithm HMM ----------------
+% FINITE or INFINITE Markov Chain?
+HMM_type = 'FINITE';
+%HMM_type = 'INFINITE';
+
+% General parameters for the Markov Chain
+if strcmp(HMM_type,'FINITE')
+    % FINITE-duration HMM
+    p_initial = [1; 0];                   % Initial probability
+    A_initial = [0.9 0.1 0; 0 0.9 0.1];   % Transition probability 
+else
+    % INFINITE-duration HMM
+    p_initial = [1; 0];                   % Initial probability
+    A_initial = [0.9 0.1;0.1 0.9];        % Transition probability
+end
+
+% Generate Markov Chain model
+mc = MarkovChain(p_initial, A_initial);   % State generator
+    
+% Define HMM with Gaussian output
+g1 = GaussD('Mean',0,'StDev',1);          % Distribution for state = 1
+g2 = GaussD('Mean',3,'StDev',2);          % Distribution for state = 2
+h  = HMM(mc, [g1; g2]);                   % The HMM
+
+% Observed finite-duration feature sequence
+x = [-0.2, 2.6, 1.3];
+
+% Get a matrix with state-conditional likelihood values
+% pX contains scaled probabilities
+pX = [g1.prob(x); g2.prob(x)];
+
+% Compute forward algorithm
+[alphaHat,c] = forward(mc,pX);
+
+% Testing logprob function 
+Prob = h.logprob(x);
+
